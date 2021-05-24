0558b6e8387e524d74ecb3ab5354d4101b0f2e52
Raza Ul Kareem <83599698+rez4president@users.noreply.github.com>
2021-05-24 10:46:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ var database = [
 				password: "supersecret",
 			},
 
+			{
+				username: "Layan",
+				password: "123",
+			},
+
+			{
+				username: "Lareen",
+				password: "456",
+			},
+
+
+
+
 		];
 
 var  newsfeed = [
@@ -23,15 +36,33 @@ var  newsfeed = [
 
 ];		
 
-var userNamePrompt = prompt("What is your username?", "");
-var passwordPrompt = prompt("What is your password?");
+/*WE USE FOR LOOP TO LOOP THROUGH THE DATABASE 
+ TO CHECK EACH LOGIN CREDENTIALTO SEE IF IT MATCHES TO
+ WHAT THE USER ENTERS!
+ SO WE CREATE A FUNCTION AND PASS TWO PARAMETERS USERNAME AND PASSWORD AND WE CHECK IF THE USERNAME AND PASSWORD BOTH MATCH THE USER, 
+ WE LET THEM ACCESS THE NEWSFEED AFTER THEY SUCCESSFULLY SIGN IN!*/
 
-function signIn(user, pass) {
-	if (user === database[0].username && database[0].password) {
+function isUserValid(username, password) {
+	for(var i = 0; i < database.length; i++) {
+		if (database[i].username === username && database[i].password === password) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function signIn(username, password) {
+	if(isUserValid(username, password)) {
 		console.log(newsfeed);
 	} else {
 		alert("Sorry, You entered the wrong credentials")
 	}
 }
 
+
+var userNamePrompt = prompt("What is your username?", "");
+var passwordPrompt = prompt("What is your password?");
+
+
+/*Always callllll the function!!!!!!*/
 signIn(userNamePrompt, passwordPrompt);
